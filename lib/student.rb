6b14a62fc450452687593586368695dc684b310d
Annia Forney <anniaforney@gmail.com>
2019-10-07 17:53:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,9 +5,9 @@ class Student
   @@all = []
 
   def initialize(hash)
-    @name = hash[":name"].to_s
-    @location = hash[":location"].to_s
-    @profile_url = hash[":profile_url"].to_s
+    @name = hash[:name].to_s
+    @location = hash[:location].to_s
+    @profile_url = hash[:profile_url].to_s
     @@all << self
   end
 
@@ -18,13 +18,13 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    @twitter = attributes_hash[":twitter"].to_s
-    @linkedin = attributes_hash[":linkedin"].to_s
-    @github = attributes_hash[":github"].to_s
-    @blog = attributes_hash[":blog"].to_s
-    
-    @profile_quote = attributes_hash[":profile_quote"].to_s
-      @bio = attributes_hash[":bio"].to_s
+    @twitter = attributes_hash[:twitter].to_s
+    @linkedin = attributes_hash[:linkedin].to_s
+    @github = attributes_hash[:github].to_s
+    @blog = attributes_hash[:blog].to_s
+
+    @profile_quote = attributes_hash[:profile_quote].to_s
+      @bio = attributes_hash[:bio].to_s
       return self
   end
 
