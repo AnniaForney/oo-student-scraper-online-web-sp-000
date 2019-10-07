aa7c014a2d415f899460d92d5b4fe41ecd594447
Annia Forney <anniaforney@gmail.com>
2019-10-07 18:06:21 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,7 +11,8 @@ class Scraper
       student_details = {}
       student_details[:name] = student.css("h4.student-name").text
       student_details[:location] = student.css("p.student-location").text
-      student_details[:profile_url] =
+      student_details[:profile_url] = './fixtures/student
+-site/index.html' + s.css("a").attribute("href")
 
           scraped_students << student_details
         end
