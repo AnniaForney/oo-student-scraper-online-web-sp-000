67ad7f55b250328123dc53b932c484cec145a867
Annia Forney <anniaforney@gmail.com>
2019-10-07 17:56:56 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,6 +11,7 @@ class Scraper
       student_details = {}
       profile_url = s.css("a").map{|link| link['href']}
       name = s.css(".card-text-container p").text.strip
+      location = s.css("p.student-location").text 
       student_details[:name] = name
       student_details[:location] = location
       student_details[:profile_url] = profile_url[0]
