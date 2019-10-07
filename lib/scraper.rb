f7e773dc871a9dd1a7e09d7417eaee72da172b58
Annia Forney <anniaforney@gmail.com>
2019-10-07 17:35:44 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,11 +4,9 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    scraped_students = []
-    page = Nokogiri::HTML open((index_url))
   end
 
   def self.create_from_collection(students_array)
-  end 
+  end
 
 end
